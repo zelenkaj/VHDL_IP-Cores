bc2e3ee42ab38e68a3ed4db48dc1c3e3a9d7534d
Vinod PA <vinodpa@kalycito.com>
2014-01-10 13:26:38 +0530

    [FEATURE] Adapt host interface ipcore release script
    - Add Xilinx-specific files
    
    Change-Id: Idcb40b1c46d563bc1dc7b915a0bb2177ac058ee9

---
+++ release_hostinterface_xilinx_ipcore.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+#
+DIR_AXI_HOSTINTERFACE="axi_hostinterface_v1_00_a"
+# clean release dir
+if [ -d release ]
+then
+    echo "clean release dir..."
+    rm -r release
+fi
+
+# create dir structure
+echo "create dir structure..."
+mkdir -p release/common/hostinterface/src
+mkdir -p release/common/lib/src
+mkdir -p release/xilinx
+mkdir -p release/xilinx/lib
+mkdir -p release/xilinx/lib/src
+mkdir -p release/xilinx/axiwrapper
+mkdir -p release/xilinx/axiwrapper/src
+mkdir -p release/xilinx/library
+mkdir -p release/xilinx/library/pcores
+mkdir -p release/xilinx/library/pcores/axi_hostinterface_v1_00_a
+mkdir -p release/xilinx/library/pcores/axi_hostinterface_v1_00_a/data
+mkdir -p release/xilinx/library/pcores/axi_hostinterface_v1_00_a/hdl
+mkdir -p release/xilinx/library/pcores/axi_hostinterface_v1_00_a/hdl/vhdl
+
+
+
+# copy docs
+#echo "copy docs..."
+#
+
+# copy Xilinx host interface
+echo "copy Xilinx Host Interface ipcore..."
+cp common/lib/src/dpRam-e.vhd                     release/common/lib/src
+cp common/lib/src/addrDecodeRtl.vhd               release/common/lib/src
+cp common/lib/src/binaryEncoderRtl.vhd            release/common/lib/src
+cp common/lib/src/cntRtl.vhd                      release/common/lib/src
+cp common/lib/src/edgedetectorRtl.vhd             release/common/lib/src
+cp common/lib/src/lutFileRtl.vhd                  release/common/lib/src
+cp common/lib/src/registerFileRtl.vhd             release/common/lib/src
+cp common/lib/src/synchronizerRtl.vhd             release/common/lib/src
+cp common/hostinterface/revision.txt                      release/common/hostinterface
+cp common/hostinterface/src/hostInterfacePkg.vhd          release/common/hostinterface/src
+cp common/hostinterface/src/hostInterfaceRtl.vhd          release/common/hostinterface/src
+cp common/hostinterface/src/irqGenRtl.vhd                 release/common/hostinterface/src
+cp common/hostinterface/src/dynamicBridgeRtl.vhd          release/common/hostinterface/src
+cp common/hostinterface/src/statusControlRegRtl.vhd       release/common/hostinterface/src
+cp common/hostinterface/src/parallelInterfaceRtl.vhd      release/common/hostinterface/src
+cp common/lib/src/global.vhd                      					release/common/lib/src
+cp xilinx/lib/src/dpRam-rtl-a.vhd								release/xilinx/lib/src
+cp xilinx/axiwrapper/src/AxiliteMasterWrapper-rtl-ea.vhd			release/xilinx/axiwrapper/src
+cp xilinx/axiwrapper/src/AxiliteSlaveWrapper-rtl-ea.vhd			release/xilinx/axiwrapper/src
+cp xilinx/library/pcores/axi_hostinterface_vX_YY_Z/data/*.*		release/xilinx/library/pcores/$DIR_AXI_HOSTINTERFACE/data
+cp xilinx/library/pcores/axi_hostinterface_vX_YY_Z/hdl/vhdl/*.*		release/xilinx/library/pcores/$DIR_AXI_HOSTINTERFACE/hdl/vhdl
